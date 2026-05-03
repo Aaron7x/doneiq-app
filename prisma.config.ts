ecaca4b3090383f05b51e3e3dd1becd1108a3e1d
Aaron7x <adring@gmail.com>
2026-05-03 13:10:02 -0700

    Permanently remove Prisma remnants

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // Make sure this is updated to the NEW password
-    url: "postgresql://postgres:GetDone2026@localhost:5432/getdone_db?schema=public&sslmode=disable",
-  },
-});
